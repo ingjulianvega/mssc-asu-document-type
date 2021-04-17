a11fec72f976d589b8d11ccf97bed7f344d5c3cd
Julian Vega <ingjulianvega@gmail.com>
2021-04-17 18:22:25 -0500

    :zap: Wrong database user. Closes #28 Wrong configuration for docker. Closes #29 New docker image 0.0.2 generated

--- src/main/resources/scripts/mysql-init.sql
+++ src/main/resources/scripts/mysql-init.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS documenttypeservice;
-DROP USER IF EXISTS `documenttypeservice`@`%`;
+DROP USER IF EXISTS `document_type_service`@`%`;
 CREATE DATABASE IF NOT EXISTS documenttypeservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `documenttypeservice`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `documenttypeservice`.* TO `documenttypeservice`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `documenttypeservice`.* TO `document_type_service`@`%`;
 FLUSH PRIVILEGES;
