f05d3fad60b6dd5f3bad768ccb6ab379197d6aed
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:21:58 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudocumenttype/exception/DocumentTypeException.java
+++ src/main/java/ingjulianvega/ximic/msscasudocumenttype/exception/DocumentTypeException.java
@@ -1,15 +1,19 @@
 package ingjulianvega.ximic.msscasudocumenttype.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DocumentTypeException extends RuntimeException {
 
-    private final String code;
-
-    public DocumentTypeException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 }
 
